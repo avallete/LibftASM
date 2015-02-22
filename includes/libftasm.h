6ee7673ee0f3d8e96a04cbbd60711eb4545fbea1
Sivenruot <sivenruot@sivenruot-GS60-2QE>
2015-02-22 13:40:39 +0100

    include

--- includes/libftasm.h
+++ includes/libftasm.h
@@ -4,4 +4,8 @@
 # include <stdlib.h>
 void	ft_bzero(void *s, size_t n);
 char	*ft_strcat(char *s1, const char *s2);
+int	ft_isdigit(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isalnum(int c);
 #endif
