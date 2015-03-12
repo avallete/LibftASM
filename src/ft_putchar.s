099bc9c45f5cdad2a58c576024bf5403e137adb3
Andrew VALLETEAU <avallete@e1r2p2.42.fr>
2015-03-12 13:07:22 +0100

    putchar more clean

--- src/ft_putchar.s
+++ src/ft_putchar.s
@@ -2,29 +2,17 @@
 %define STDOUT				1
 %define	WRITE				4
 
-section .data
-	buf:
-		.byte db 0
-
 section	.text
 	global	ft_putchar									;int	ft_putchar(char c)
 
 ft_putchar:
-	push	rsi										;save rsi addr
-	push	rdx										;save rdx addr
-	push	rdi										;save rdi addr
-	push 	rcx
-	mov		rdx,	1								;write len 1
-	lea		rcx,	[rel buf.byte]							;move in rcx buf byte
-	mov 		[rcx],	dil								;move byte rdi in rcx value
-	mov		rsi,	rcx								;move rcx in rsi for write
-	mov		rdi,	STDOUT								;init fd for STDOUT
-	mov		rax,	MACH_SYSCALL(WRITE)						;syscall write
+	push	rdi										;save rsi addr
+	mov		rsi,	rsp								;move stack ptr on rsi
+	mov		rdx,	1								;len is 1
+	mov		rdi,	1								;fd
+	mov		rax,	MACH_SYSCALL(WRITE)				;syscall write
 	syscall
 
 exit:
-	pop 	rcx
 	pop	rdi
-	pop	rdx										;Restore registre addr
-	pop	rsi
 	ret
